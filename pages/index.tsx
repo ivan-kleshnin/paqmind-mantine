556a8f667e939339f41e0d4cc4121d1208fc71d6
Ivan Kleshnin <ivan@paqmind.com>
2022-07-01 13:24:43 +0200

    Add hero section background

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
         </Navbar>
       }
       footer={<AppFooter/>}
-      sx={{
+      styles={{
         main: {
           display: opened ? "none" : undefined,
           [theme.fn.largerThan("sm")]: {
@@ -50,11 +50,11 @@ export default function HomePage() {
           background: theme.colors.gray[0],
           minHeight: "calc(100vh - 5rem)",
           padding: 0,
-          marginTop: "1rem",
         },
       }}
     >
-      <Container size="lg" mt="5rem">
+      <Background/>
+      <Container size="lg">
         <h1>First</h1>
         <p>First</p>
         <Code block>{`
@@ -106,7 +106,7 @@ console.log("Hi!")
         <p>Test</p>
         <p>Test</p>
         <p>Test</p>
-        <Button>Mantine!</Button>
+        <Button mb="2rem">Mantine!</Button>
       </Container>
     </AppShell>
   </>
@@ -182,3 +182,20 @@ function AppFooter() {
 }
 
 // https://github.com/mantinedev/mantine/discussions/680
+
+function Background() {
+  const height = "12rem"
+  return <Box mt="5rem" sx={{height}}>
+    <Container size="lg">
+      <h1 style={{fontSize: "60px", position: "absolute", color: "white"}}>HERO SECTION</h1>
+    </Container>
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+      <defs>
+        <pattern id="brick" width="200" height="200" patternUnits="userSpaceOnUse">
+          <rect fill="#487346" width="200" height="200"/><g fillOpacity="1"><polygon fill="#4c8e43" points="100 57.1 64 93.1 71.5 100.6 100 72.1"/><polygon  fill="#6aac5f" points="100 57.1 100 72.1 128.6 100.6 136.1 93.1"/><polygon  fill="#4c8e43" points="100 163.2 100 178.2 170.7 107.5 170.8 92.4"/><polygon  fill="#6aac5f" points="100 163.2 29.2 92.5 29.2 107.5 100 178.2"/><path  fill="#89CC7C" d="M100 21.8L29.2 92.5l70.7 70.7l70.7-70.7L100 21.8z M100 127.9L64.6 92.5L100 57.1l35.4 35.4L100 127.9z"/><polygon  fill="#768c3a" points="0 157.1 0 172.1 28.6 200.6 36.1 193.1"/><polygon  fill="#96ac58" points="70.7 200 70.8 192.4 63.2 200"/><polygon  fill="#B6CC76" points="27.8 200 63.2 200 70.7 192.5 0 121.8 0 157.2 35.3 192.5"/><polygon  fill="#96ac58" points="200 157.1 164 193.1 171.5 200.6 200 172.1"/><polygon  fill="#768c3a" points="136.7 200 129.2 192.5 129.2 200"/><polygon  fill="#B6CC76" points="172.1 200 164.6 192.5 200 157.1 200 157.2 200 121.8 200 121.8 129.2 192.5 136.7 200"/><polygon  fill="#768c3a" points="129.2 0 129.2 7.5 200 78.2 200 63.2 136.7 0"/><polygon  fill="#B6CC76" points="200 27.8 200 27.9 172.1 0 136.7 0 200 63.2 200 63.2"/><polygon  fill="#96ac58" points="63.2 0 0 63.2 0 78.2 70.7 7.5 70.7 0"/><polygon fill="#B6CC76" points="0 63.2 63.2 0 27.8 0 0 27.8"/></g>
+        </pattern>
+      </defs>
+      <rect width="100%" height={height} fill="url(#brick)"></rect>
+    </svg>
+  </Box>
+}
